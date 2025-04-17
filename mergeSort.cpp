36fde91cdab71efd10a3390a1747992ffbaddae4
upi1682 <upi538287@gmail.com>
2025-04-17 18:16:41 +0700

    step1.

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -4,17 +4,10 @@ using namespace std;
 int arr[20], B[20]; // Array utama dan Array bantu
 int n;
 
-void input()
-{
-    while (true) 
-    {
-        cout << "Masukan Panjang element array : ";
-        cin >> n;
-        if (n <= 20)
-        break;
-        cout << "\nMaksimal panjang array adalah 20" << endl;
+
+
+void mergeSort (int low, int high) {
+    if (low >= high){ //step1
+        return; // step 1a
     }
-
-
-    
 }
